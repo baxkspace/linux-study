7af0b2abf74c2a988b5fdefc3d586d8f023d1fa1
baxkspace <baxkspace@knu.ac.kr>
2023-05-08 23:56:05 +0900

    feat: hw8-shell

---
+++ ch09/HW#8/smsh.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include "smsh.h"
+
+#define DFL_PROMPT	"> "
+
+int main(){
+	int background = 0, pid;
+	char *cmdline, *prompt, **arglist;
+	int result;
+	void setup();
+
+	prompt = DFL_PROMPT;
+	setup();
+	while((cmdline = next_cmd(prompt, stdin, &background)) != NULL){
+		if((arglist = splitline(cmdline)) != NULL){
+			if (strcmp(arglist[0], "exit") == 0){
+				kill(pid, SIGINT);
+				exit(0);
+			}
+			else{
+				result = execute(arglist, background, &pid);
+				freelist(arglist);
+			}
+		}
+		background = 0;
+		free(cmdline);
+	}
+	return 0;
+}
+
+void setup(){
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void fatal(char *s1, char *s2, int n){
+	fprintf(stderr, "Error: %s, %s\n", s1, s2);
+	exit(n);
+}
