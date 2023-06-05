93bb703b37e876fd282f6387eb56a283b26b7b09
baxkspace <baxkspace@knu.ac.kr>
2023-06-05 09:16:55 +0900

    feat: lab12 webserv

---
+++ ch12/lab12/socklib.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+#include <strings.h>
+
+int make_server_socket(int);
+int make_server_socket_q(int, int);
+int connect_to_server(char*, int);
+
